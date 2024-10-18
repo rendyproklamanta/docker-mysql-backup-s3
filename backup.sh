c1d98e79457551508e9476adc8c4525be9ea3e72
Rendy Dwi Proklamanta <rendyproklamanta@gmail.com>
2024-10-18 18:16:29 +0700

    update

--- backup.sh
+++ backup.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
 
+DB_HOST=$(cat "$DB_HOST_FILE")
+DB_PORT=$(cat "$DB_PORT_FILE")
+DB_USER=$(cat "$DB_USER_FILE")
+DB_PASS=$(cat "$DB_PASS_FILE")
+
 # Set variables
 DATE=$(date +%Y%m%d%H%M%S)
 BACKUP_DIR="/tmp/backup/mysql"
+ENCRYPTION_FILE="$BACKUP_DIR/encryption_$DATE.tar.gz"
 BACKUP_FILE="$BACKUP_DIR/all_db_backup_$DATE.sql.gz"
 BINLOG_BACKUP_FILE="$BACKUP_DIR/binlog_backup_$DATE.tar.gz"
-ENDPOINT_URL="https://s3.${AWS_DEFAULT_REGION}.backblazeb2.com"  # Backblaze region
 
 # Create backup directory if it doesn't exist
 mkdir -p $BACKUP_DIR
 
+# Create a compressed archive of the backup directory
+tar -czf $ENCRYPTION_FILE -C $ENCRYPTION_SOURCE .
+
+# SSL-related parameters
+SSL_PARAMS=""
+if [ "$ENABLE_SSL" = "true" ]; then
+    SSL_PARAMS="--ssl --ssl-ca=/etc/my.cnf.d/tls/ca-cert.pem --ssl-cert=/etc/my.cnf.d/tls/client-cert.pem --ssl-key=/etc/my.cnf.d/tls/client-key.pem"
+fi
+
 # Perform MySQL dump for all databases, including routines and events
-mysqldump -h$DB_HOST -P$DB_PORT -u$DB_USER -p$DB_PASSWORD --all-databases --routines --events | gzip > $BACKUP_FILE
+mysqldump -h$DB_HOST -P$DB_PORT -u$DB_USER -p$DB_PASS --all-databases --routines --events $SSL_PARAMS | gzip > $BACKUP_FILE
 
 # Backup binary logs
 mysqlbinlog --read-from-remote-server --raw --stop-never mysql-bin.* > $BINLOG_BACKUP_FILE
@@ -19,10 +33,12 @@ mysqlbinlog --read-from-remote-server --raw --stop-never mysql-bin.* > $BINLOG_B
 # Upload MySQL dump and binary logs to Backblaze S3-Compatible bucket
 aws s3 cp $BACKUP_FILE $S3_BUCKET/ --endpoint-url $ENDPOINT_URL
 aws s3 cp $BINLOG_BACKUP_FILE $S3_BUCKET/ --endpoint-url $ENDPOINT_URL
+aws s3 cp $ENCRYPTION_FILE $S3_BUCKET/ --endpoint-url $ENDPOINT_URL
 
 # Remove local backup files
 rm -f $BACKUP_FILE
 rm -f $BINLOG_BACKUP_FILE
+rm -f $ENCRYPTION_FILE
 
 # Delete old backups in Backblaze S3-Compatible bucket older than 5 days
 aws s3 ls $S3_BUCKET/ --endpoint-url $ENDPOINT_URL | while read -r line; do
--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,9 @@ CRON_SCHEDULE="${CRON_SCHEDULE:-0 */4 * * *}"
 # Write out the cron job to file
 echo "$CRON_SCHEDULE /usr/local/bin/backup.sh >> /var/log/backup.log 2>&1" > /etc/crontabs/root
 
+# Execute backup in entrypoint
+cd /usr/local/bin
+./backup.sh
+
 # Start cron in foreground
 crond -f -l 2
